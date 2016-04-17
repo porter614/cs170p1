523c3b1e949d1f46d4301b779386b11b207d2f79
nadoora <nader@umail.ucsb.edu>
2016-04-16 23:39:40 -0700

    Update shell.c
    New function correct_string:
    
    everything compiles, functionality errors tho: 
    - does not work twice (works for only one command... must not be resetting something)
    - does not work for the case where the operator is stuck to the 2nd item:
    3 cases: 
    sish:> command <file // <FAILS>
    sish:> command< file // <PASS>
    sish:> command < file // <PASS>

--- shell.c
+++ shell.c
@@ -8,6 +8,7 @@
 void parse_path();
 void parse_input(char * buff);
 int count_it( char* str, char del );
+void correct_string(char* str);
 
 char** paths;
 int c;
@@ -93,9 +94,95 @@ void parse_input(char * buff)
 	// printf("%s\n", buff);
 }
 
-//function in: input, out: corrected string
+void correct_string(char* str)
+{
+	//make local copy
+	char** corrected_str;
+	int corrected_args = 0;
 
-// separate string into array of tokens/operators
+	char** args;
+	char copy[100];
+	strcpy(copy, str);
 
-// recreate string of array concat. by spaces
+	int num_args = count_it(copy, ' ');
+	args = malloc(num_args*sizeof(char*)); //2* bcs thats the max num of operators, then search for operators
+	corrected_str = malloc(2*num_args*sizeof(char*)); //2* bcs thats the max num of operators, then search for operators
+
+	int i = 0;
+	args[i] = strtok(copy, " ");
+	while( i < num_args )
+	{
+		i += 1;
+		args[i] = strtok(NULL, " ");
+	}
+
+	const char* operators = "<>|";
+	int boolean = 0;
+	for(i = 0; i < num_args; i++)
+	{
+		char* c = args[i];
+		while( *c )
+		{		
+			// add the term, operator, term
+			if( strchr( operators , *c) )
+			{
+				// printf("%c is in \"%s\" @index=%d\n", *c, args[i], (int)(c-args[i]) );
+
+				//get command1
+				strncpy(corrected_str[corrected_args], args[i], (int)(c-args[i])  );
+				corrected_str[corrected_args][(int)(c-args[i])] = '\0';
+				corrected_args += 1;
+				printf("%s", args[i]);
+
+				//get operator
+				int index = (int)(c-args[i]);	
+				printf("Index: %d\n", index);
+				char* temp = args[i];
+				temp = temp+5;
+				printf("FromCharacterOn: %s", temp);
+				// char subbuff [ strlen(args[i]) - index  ];
+				// memcpy( subbuff, args[] )
+				// printf("FromCharacterOn: %s", args[i][index]);
+				strncpy(corrected_str[corrected_args], temp, 1  );
+				corrected_str[corrected_args][1] = '\0';
+				corrected_args += 1;	
+
+				//get 2ndcommand if any
+				// strncpy(corrected_str[corrected_args], args[i]+(int)(c-args[i])+1, 5 );
+				int len_last_term = strlen( args[i] )-(int)( c-args[i] )  ;
+				char* mod_str = c+1;
+				printf("\nMod: %s\n", mod_str);
+				// strncpy(corrected_str[corrected_args], (char*)(c-args[i]) , len_last_term );
+				corrected_str[corrected_args] = mod_str;
+				corrected_args += 1;				
+
+				boolean = 1;
+			}
+			c++; //hehe
+		}
+
+		if(boolean)
+		{
+			boolean = 0;
+			
+		}
+		else
+		{
+			printf("No Arg: %s", args[i]);
+			corrected_str[ corrected_args ] = args[i];
+			corrected_args+=1;
+			
+		}
+
+	}//end for
+
+	printf(":: PRINTING CORRECTED ARRAY OF ARGS ::\n\n");
+	int j = 0;
+	for(; j < corrected_args; j++)
+	{
+		printf("%s\n", corrected_str[j]);
+	}
+
+
+}//endfunc
 
