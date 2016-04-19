9f71219854792f5f3ca71a85fb1dda2b8331a514
Victor Porter <vporter@umail.ucsb.edu>
2016-04-18 22:05:39 -0700

    Redirection works for greater than pipes

--- shell.c
+++ shell.c
@@ -3,16 +3,19 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
-void launch(char **args);
+void launch(char **args, int num_args, int gt_found);
 void print_command(char **args, int num_args);
 
 int main()
 {
 	char c;
-	char* current_arg = malloc(50*sizeof(char*)); //max size of current_arg = 50 chars (arbitrary max)
-	char** command_args = malloc(1000*sizeof(char*)); // literally no clue what to set it as
-	int index = 0, num_args = 0;
+	char* current_arg = malloc(50*sizeof(char)); //max size of current_arg = 50 chars (arbitrary max)
+	char* last_arg = malloc(50*sizeof(char));
+	char** command_args = malloc(50*sizeof(char*)); // literally no clue what to set it as
+	char** launch_command = malloc(50*sizeof(char*));
+	int index = 0, num_args = 0, pipe_indeces[50], pipe_count = 0, gt_found = 0, lt_found;
 
 	printf("\nsish:>");
 	while( (c=getchar())!= EOF )
@@ -36,7 +39,7 @@ int main()
 
 				print_command(command_args, num_args);
 				//fork/execute progam
-				launch(command_args);
+				launch(command_args, num_args, gt_found);
 
 
 				//Reset Variables for next command
@@ -47,20 +50,31 @@ int main()
 			}
 			else if( c == ' ') continue; //If we hit a space, reset arg index
 
-			else if( c == '|') {
-				command_args[ num_args ] = '\0';
-			}
-
-
+			// else if( c == '|') {
+			// 	command_args[ num_args ] = '\0';
+			// }
 			else { // c == | < > &
 				char* charstr = malloc(2*sizeof(char*));
+				command_args[ num_args ] =  malloc(2*sizeof(char*));
 				charstr[0] = c;
 				charstr[1] = '\0';
 
-				command_args[ num_args ] =  malloc(2*sizeof(char*));
-				strcpy(command_args[ num_args ], charstr); //store the operator as a current_arg
-				num_args += 1;
-				free(charstr);
+				// strcpy(command_args[ num_args ], charstr); //store the operator as a current_arg
+				// num_args += 1;
+				// free(charstr);
+
+				// if( c == '|') {
+				// 	pipe_indeces[pipe_count] = num_args - 1;
+				// 	printf("Pipe at arg %d\n", pipe_indeces[pipe_count]);
+				// 	pipe_count += 1;
+				// }
+				if(c == '>') {
+					gt_found = 1;					
+				}
+				else if (c == '<') {
+					lt_found = 1;
+				}
+
 			}
 		}
 		else //Reached normal character
@@ -73,14 +87,22 @@ int main()
 }
 
 
-void launch(char **args) {
-	pid_t process_id = fork();	
-	int status;
+void launch(char **args, int num_args, int gt_found) {	
+	int status, fp = 1, stdout_save;
 
+	if (gt_found) {
+		printf("Last arg = %s", args[num_args - 1]);
+		fp = open(args[num_args - 1], O_WRONLY); //Open file/get file descriptor
+		args[num_args - 1] = '\0'; 				 //Delete file from command so you can run
+
+		stdout_save = dup(1);  //duplicate stdout fd
+	    dup2(fp, 1);		   //make stdout point to file pointer
+    }
+
+
+    pid_t process_id = fork();
 	if (process_id == 0) {
 		//Child Process
-		printf("Hello from Child\n");
-
 		if (execvp(args[0], args) == -1) {
 		//if (execvp("/bin/sh", NULL) == -1){
 		    printf("Unknown command\n");
@@ -92,7 +114,6 @@ void launch(char **args) {
 	}
 	else {
 		//Parent Process
-		printf("Hello from Parent\n");
 		int child_return_status;
 		waitpid(process_id, &child_return_status, 0);
 
@@ -102,25 +123,56 @@ void launch(char **args) {
 		}
 		else {
 			//no error
+
+			if ( gt_found ) {
+				dup2(stdout_save, 1);
+				close(stdout_save);
+				close(fp);
+			}
 			printf("Child process finished with return status %d\n", child_return_status);
+
 		}
 	}
 }
 
 void launch_pipe(char **args1, char **args2) {	
-	int status, pipe_fd[2];
+	// int pipe_fd[2];
 
-	if (fork() == 0) {
-		if (execvp(args[0], args) == -1) {
+	// if (pipe(pipefd) == -1) {
+ //        perror("pipe");
+ //        exit(EXIT_FAILURE);
+ //    }
+ //    pid_t p_id = fork();
 
-		}
-	}
-	else if (process_id < 0) {
-		//error
-	}
-	else {
+ //    if (process_id == 0) {
+	// 	//Child Process
+	// 	if (execvp(args[0], args) == -1) {
+	// 	//if (execvp("/bin/sh", NULL) == -1){
+	// 	    printf("Unknown command\n");
+	// 	    exit(0);
+	// 	}
+	// }
+	// else if (process_id < 0) {
+	// 	//error
+	// }
+	// else {
+	// 	//Parent Process
+	// 	printf("Hello from Parent\n");
+	// 	int child_return_status;
+	// 	waitpid(process_id, &child_return_status, 0);
 
-	}
+	// 	if (child_return_status){
+	// 		//error
+	// 		printf("Error in child process.");
+	// 	}
+	// 	else {
+	// 		//no error
+	// 		printf("Child process finished with return status %d\n", child_return_status);
+	// 	}
+	// }
+
+
+
 }
 
 
